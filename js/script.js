ac6edad4dfa556c666672d7cc13321c6c9052896
OsmanMariam <sufimariam8@gmail.com>
2019-10-07 19:16:30 +0300

    add toggle effect on images

--- js/script.js
+++ js/script.js
@@ -2,7 +2,50 @@ console.log('osman');
 console.log('mariam');
 console.log(69);
 
+$(document).ready(function() {
 
+  $(".whatwedo p").hide();
+
+  $('.whatwedo img.one').click(function() {
+    $(".whatwedo img.one").toggle();
+    $(".whatwedo p.one").toggle();
+  });
+  $('.whatwedo p.one').click(function() {
+    $(".whatwedo img.one").toggle();
+    $(".whatwedo p.one").toggle();
+  });
+  $('.whatwedo img.two').click(function() {
+    $(".whatwedo img.two").toggle();
+    $(".whatwedo p.two").toggle();
+  });
+  $('.whatwedo p.two').click(function() {
+    $(".whatwedo img.two").toggle();
+    $(".whatwedo p.two").toggle();
+  });
+  $('.whatwedo img.three').click(function() {
+    $(".whatwedo img.three").toggle();
+    $(".whatwedo p.three").toggle();
+  });
+  $('.whatwedo p.three').click(function() {
+    $(".whatwedo img.three").toggle();
+    $(".whatwedo p.three").toggle();
+  });
+
+  $('.whatwedo h3').click(function() {
+    $(".whatwedo img.one").toggle();
+    $(".whatwedo p.one").toggle();
+  });
+  $('.whatwedo h3').click(function() {
+    $(".whatwedo img.one").toggle();
+    $(".whatwedo p.one").toggle();
+  });
+
+  $("table").hide();
+  $(".additional-buttons").hide();
+  $(".additional-info").hide();
+  $(".btn.yes").hide();
+  $(".btn.no").hide();
+  $(".additional-info h4").hide();
 
 
 
