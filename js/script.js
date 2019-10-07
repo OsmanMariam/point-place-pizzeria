e13157042c934453a437759b8f437bfd61fa8b51
OsmanMariam <sufimariam8@gmail.com>
2019-10-07 19:18:11 +0300

    add function to calculate total cost

--- js/script.js
+++ js/script.js
@@ -47,7 +47,22 @@ $(document).ready(function() {
   $(".btn.no").hide();
   $(".additional-info h4").hide();
 
+  $('.btn.order').click(function() {
+    var sizeOfPizza = $(".size option:selected").val();
+    var toppingsOfPizza = $(".toppings option:selected").val();
+    var crustOfPizza = $(".crust option:selected").val();
+    var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+    var order = 1;
+    var grandTotal = 0;
 
+    $("table").show();
+    $(".additional-buttons").show();
+    $(".btn.order").hide();
+
+    $("#size").html($(".size option:selected").text() + " - " + sizeOfPizza);
+    $("#toppings").html($(".toppings option:selected").text() + " - " + toppingsOfPizza);
+    $("#crust").html($(".crust option:selected").text() + " - " + crustOfPizza);
+    $("#total").html(total);
 
 
 
